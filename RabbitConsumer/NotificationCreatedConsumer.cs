efec4704c7ae2d6a02e031c3d71edbe4df713555
Кирилл Федорин <kirill.fedorin@mail.ru>
2022-12-17 10:56:09 +0300

    first

--- RabbitConsumer/NotificationCreatedConsumer.cs
+++ RabbitConsumer/NotificationCreatedConsumer.cs
@@ -3,13 +3,13 @@ using MassTransit;
 
 namespace RabbitConsumer
 {
-    public class NotificationCreatedConsumer : IConsumer<INotificationCreated>
+    public class NotificationCreatedConsumer //: IConsumer<INotificationCreated>
     {
-        public async Task Consume(ConsumeContext<INotificationCreated> context)
-        {
-            var serializedMessage = JsonSerializer.Serialize(context.Message, new JsonSerializerOptions { });
+        //public async Task Consume(ConsumeContext<INotificationCreated> context)
+        //{
+        //    var serializedMessage = JsonSerializer.Serialize(context.Message, new JsonSerializerOptions { });
 
-            Console.WriteLine($"NotificationCreated event consumed. Message: {serializedMessage}");
-        }
+        //    Console.WriteLine($"NotificationCreated event consumed. Message: {serializedMessage}");
+        //}
     }
 }
