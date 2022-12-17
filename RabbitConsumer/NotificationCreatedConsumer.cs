60f0c969bdc9048dca1021a22ff2a5f5bf92e565
Кирилл Федорин <kirill.fedorin@mail.ru>
2022-12-17 10:55:06 +0300

    Add project files.

---
+++ RabbitConsumer/NotificationCreatedConsumer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using MassTransit;
+
+namespace RabbitConsumer
+{
+    public class NotificationCreatedConsumer : IConsumer<INotificationCreated>
+    {
+        public async Task Consume(ConsumeContext<INotificationCreated> context)
+        {
+            var serializedMessage = JsonSerializer.Serialize(context.Message, new JsonSerializerOptions { });
+
+            Console.WriteLine($"NotificationCreated event consumed. Message: {serializedMessage}");
+        }
+    }
+}
---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    services.AddMassTransit(busConfigurator =>
+    {
+        var entryAssembly = Assembly.GetExecutingAssembly();
+
+        busConfigurator.AddConsumers(entryAssembly);
+        busConfigurator.UsingRabbitMq((context, busFactoryConfigurator) =>
+        {
+            busFactoryConfigurator.Host("rabbitmq", "/", h => { });
+
+            busFactoryConfigurator.ConfigureEndpoints(context);
+        });
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
